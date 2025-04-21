5f0aa738009717c1cb50bdd5d75506d880ed571c
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-21 15:19:58 +0500

    Update

---
+++ script.js
@@ -0,0 +1,33 @@
+var tl = gsap.timeline()
+
+
+tl.to("#yellow", {
+    top: "-100%", 
+    delay: 0.5,
+    duration: 0.5,
+    ease: "expo.out"
+})
+
+tl.to("#yellow1",{
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.7,
+    ease: "expo.out"
+}) 
+
+tl.from("#yellow2",{
+    top: "100%",
+    delay: 0.5,
+    duration: 0.7,
+    ease: "expo.out"
+}, "anim") 
+
+tl.to("#loader h1", {
+    delay: 0.6,
+    duration: 0.7,
+    color: "black",
+}, "anim")
+
+tl.to("#loader", {
+    opacity:0
+})
