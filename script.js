b8a79daec64b91ba3cce44892d1fe599d6a8f050
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-22 12:39:06 +0500

    Update

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 function loading() {
     var tl = gsap.timeline()
 
-tl.to("#yellow", {
-    top: "-100%", 
-    delay: 0.5,
-    duration: 0.5,
-    ease: "expo.out"
-})
+    tl.to("#yellow", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.5,
+        ease: "expo.out"
+    })
 
-tl.to("#yellow1",{
-    top: "-100%",
-    delay: 0.5,
-    duration: 0.7,
-    ease: "expo.out"
-}) 
+    tl.to("#yellow1", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.7,
+        ease: "expo.out"
+    })
 
-tl.from("#yellow2",{
-    top: "100%",
-    delay: 0.5,
-    duration: 0.7,
-    ease: "expo.out"
-}, "anim");
+    tl.from("#yellow2", {
+        top: "100%",
+        delay: 0.5,
+        duration: 0.7,
+        ease: "expo.out"
+    }, "anim");
 
-tl.to("#loader h1", {
-    delay: 0.6,
-    duration: 0.7,
-    color: "black",
-}, "anim");
+    tl.to("#loader h1", {
+        delay: 0.6,
+        duration: 0.7,
+        color: "black",
+    }, "anim");
 
-tl.to("#loader", {
-   opacity: 0
-})
+    tl.to("#loader", {
+        opacity: 0
+    })
 
-tl.to("#loader", {
-   display: "none"
-})
+    tl.to("#loader", {
+        display: "none"
+    })
 }
 
 loading()
@@ -44,11 +44,19 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function loco() {
+    document.querySelector("#footer h2").addEventListener("click", () => {
+        scroll.scrollTo(0)
+    })
+}
+
+loco()
+
 var elems = document.querySelectorAll(".elem")
 var page2 = document.querySelector("#page2")
-elems.forEach(function(ele){
-    ele.addEventListener("mouseenter", function() {
-      var bgimg = ele.getAttribute("data-img")
-      page2.style.backgroundImage = `url(${bgimg})`
+elems.forEach(function (ele) {
+    ele.addEventListener("mouseenter", function () {
+        var bgimg = ele.getAttribute("data-img")
+        page2.style.backgroundImage = `url(${bgimg})`
     })
 })
